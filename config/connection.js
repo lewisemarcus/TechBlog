38c4c142fc61c53b18b1fae11fcab4f252bfe7ba
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-03-28 20:10:00 -0700

    connected to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
+import sequelize from '../../E-CommerceBackEnd/config/connection';
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'https://agile-dawn-55678.herokuapp.com',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+if (process.env.JAWSDB_URL) {
+  const sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
